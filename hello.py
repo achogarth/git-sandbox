284ca5464febafbae1dc431be56cc229c0e798d1
achogarth <achogarth@gmail.com>
2020-07-26 22:02:25 -0500

    changed hello.py main

--- hello.py
+++ hello.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 def hello(s):
-    print ("Hello ", s)
+    print ("Hello", s)
 
 def hellocall():
     print("Hello from module")
 
 if __name__ == "__main__":
-    import sys
-    hello(sys.argv[1])
+    print("Hello Main")
+    hello("World")
+    hellocall()
